705294982d15c71171fe0d39684d74de33159818
Rupa Dhillon <rupa211984@gmail.com>
2019-06-14 17:00:53 -0700

    Score spaces for AI recursively

--- computerPlayer.js
+++ computerPlayer.js
@@ -18,10 +18,10 @@ class ComputerPlayer {
     });
   }
 
-  emptySpaces() {
+  emptySpaces(board = this.board) {
     let spaces = [];
 
-    this.board.board.forEach((row, y) => {
+    board.board.forEach((row, y) => {
       row.forEach((space, x) => {
         if (space == null) spaces.push([y, x]);
       });
@@ -31,59 +31,52 @@ class ComputerPlayer {
   }
 
   choose() {
-    return this.findWin(this.marker) ||
-      this.findWin(this.oppMarker) || 
-      this.center() ||
-      this.findCorner() ||
-      this.randomSpace();
-  }
+    let bestSpace;
+    let bestScore = null;
 
-  findWin(marker) {
-    const deepDup = function (arr) {
-      if (!(arr instanceof Array)) return arr;
-      return arr.map((el) => deepDup(el));
-    };
-
-    const boardCopy = new Board();
-    let pos;
-
-    for (let y = 0; y < 3; y++) {
-      for (let x = 0; x < 3; x++) {
-        boardCopy.board = deepDup(this.board.board);
-        pos = [y, x];
-        boardCopy.placeMark(pos, marker);
-        if (boardCopy.won()) return pos;
-      }
-    }
-
-    return null;
-  }
-
-  findCorner() {
-    const neighbors = [
-      [[0, 0], [0, 1], [1, 0]], 
-      [[0, 2], [0, 1], [1, 2]], 
-      [[2, 0], [1, 0], [2, 1]], 
-      [[2, 2], [2, 1], [1, 2]]
-    ];
-
-    for (let i = 0; i < neighbors.length; i++) {
-      if (this.board.isEmpty(neighbors[i][0])) {
-        if (neighbors[i].slice(1).some(pos => this.board.board[pos[0]][pos[1]] === this.oppMarker)) {
-          return neighbors[i][0];
-        }
-      }
-    }
-
-    return [[0, 0], [0, 2], [2, 0], [2, 2]].find(pos => this.board.isEmpty(pos));
-  }
-
-  center() {
-    return (this.board.isEmpty([1, 1])) ? [1, 1] : null;
-  }
-
-  randomSpace() {
     const empties = this.emptySpaces();
+
+    if (empties.length === 9) {
+      return this.randomSpace([[1, 1], [0, 0], [0, 2], [2, 0], [2, 2]]);
+    }
+
+    empties.forEach((coords => {
+      let score = this.scoreSpace(coords);
+      
+      if (bestScore === null || (score > bestScore)) {
+        bestScore = score;
+        bestSpace = coords;
+      }
+    }));
+
+    return bestSpace;
+  }
+
+  scoreSpace(pos, board = this.board, marker = this.marker, weight = 100) {
+    const bcopy = new Board();
+    bcopy.board = this.deepDup(board.board);
+    bcopy.placeMark(pos, marker);
+    
+    if (bcopy.won()) {
+      return (bcopy.winner() === this.marker) ? 1 * weight : -1 * weight;
+    }
+
+    let score = 0;
+    marker = (marker === this.marker) ? this.oppMarker : this.marker;
+
+    this.emptySpaces(bcopy).forEach((coords) => {
+      score += this.scoreSpace(coords, bcopy, marker, weight / 2);
+    });
+
+    return score;
+  }
+
+  deepDup(arr) {
+    if (!(arr instanceof Array)) return arr;
+    return arr.map((el) => this.deepDup(el));
+  }
+
+  randomSpace(empties) {
     const choice = Math.floor(Math.random() * empties.length);
     return empties[choice];
   }
